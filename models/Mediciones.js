06585e9bee9f95d4ea50fbe92428987f253b6715
alangohe845 <alangomez845@gmail.com>
2023-04-19 23:44:33 -0600

    vf_socket

--- models/Mediciones.js
+++ models/Mediciones.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const socketIo = require("socket.io")
 const medicionesSchema= new mongoose.Schema({
       id: {
         type: String,
@@ -7,6 +7,22 @@ const medicionesSchema= new mongoose.Schema({
       RFID: {
         type: String,
       },
+});
+
+const server = require('http').createServer();
+const io = socketIo(server);
+
+io.listen(3000);
+io.on('coneccion', (socket) => {
+  const changeStream = medicionesSchema.watch();
+
+  changeStream.on('change', (change) => {
+    socket.emit('update', change.fullDocument);
+  });
+  socket.on('disconect', () => {
+    changeStream.close()
+  });
+
 });
 
 module.exports = mongoose.model("mediciones", medicionesSchema, "lector"); //Ultimo parametro nombre de la coleccion
